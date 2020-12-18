c8610f1a8b195b7c6e19ee3d3735356c0d4f3773
Tharun sekar <tharunsd23@gmail.com>
2020-12-18 13:58:43 +0530

    sa

---
+++ build/precache-manifest.1f023e327238fa0e8265ab3ddca5ac2a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "279efea31b85580d516e",
+    "url": "/Tharun2331/react-final/static/css/main.eb3927e4.chunk.css"
+  },
+  {
+    "revision": "279efea31b85580d516e",
+    "url": "/Tharun2331/react-final/static/js/main.9926e90b.chunk.js"
+  },
+  {
+    "revision": "bab0408288a0bec049de",
+    "url": "/Tharun2331/react-final/static/js/runtime~main.73c8e44c.js"
+  },
+  {
+    "revision": "bde40ea9039333c3849e",
+    "url": "/Tharun2331/react-final/static/js/2.996648fb.chunk.js"
+  },
+  {
+    "revision": "61af556ee3db4cc28f57",
+    "url": "/Tharun2331/react-final/static/css/3.b3ee4cc5.chunk.css"
+  },
+  {
+    "revision": "61af556ee3db4cc28f57",
+    "url": "/Tharun2331/react-final/static/js/3.a3ecdb85.chunk.js"
+  },
+  {
+    "revision": "ae267033b2ff3942ee15",
+    "url": "/Tharun2331/react-final/static/js/4.ae8be2ea.chunk.js"
+  },
+  {
+    "revision": "564b28933eb27c576a94",
+    "url": "/Tharun2331/react-final/static/js/5.2e05af7f.chunk.js"
+  },
+  {
+    "revision": "3fb0c2c28d1b85e008d7",
+    "url": "/Tharun2331/react-final/static/css/6.c9ca40bf.chunk.css"
+  },
+  {
+    "revision": "3fb0c2c28d1b85e008d7",
+    "url": "/Tharun2331/react-final/static/js/6.b712ba35.chunk.js"
+  },
+  {
+    "revision": "a628627fc5db371dceff",
+    "url": "/Tharun2331/react-final/static/css/7.bd47544b.chunk.css"
+  },
+  {
+    "revision": "a628627fc5db371dceff",
+    "url": "/Tharun2331/react-final/static/js/7.2dc0e216.chunk.js"
+  },
+  {
+    "revision": "bcaeddde107575ae6ce2",
+    "url": "/Tharun2331/react-final/static/js/8.543a0219.chunk.js"
+  },
+  {
+    "revision": "0f7b1350be27582e44ff4b8faad4e13e",
+    "url": "/Tharun2331/react-final/static/media/crown.0f7b1350.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/Tharun2331/react-final/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "4c97f362f97e093c43207d1cad3268c1",
+    "url": "/Tharun2331/react-final/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Tharun2331/react-final/precache-manifest.1f023e327238fa0e8265ab3ddca5ac2a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Tharun2331/react-final/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
